feec9060f974f075710c9dfd8f3bc4acef9b0e35
Neha <mintuerugadindla@gmail.com>
2026-04-03 16:05:11 +0530

    connected backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL || '"https://movie-booking-system-ja3o.onrender.com/api",
   headers: { 'Content-Type': 'application/json' },
   timeout: 15000, // 15s timeout
 });
